f2300b13822ae13d13e9fc16e343801b675c6e89
Marek Grzybek <it.doesnt.matter.blog@gmail.com>
2019-02-25 02:12:30 +0100

    Use image with corrected perspective

--- snooker-tracker.py
+++ snooker-tracker.py
@@ -8,16 +8,20 @@ from colorfinder import ColorFinder
 
 
 def main():
-    img = load_image('3.png')
+    img = load_image('1.png')
     img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    table_contour = outline_table( img, get_table_coords(img_gray) )
+    img_corrected = correct_perspective(img, table_contour)
+    img_gray = cv.cvtColor(img_corrected, cv.COLOR_BGR2GRAY)
 
-    balls = mark_balls(img,
+    balls = mark_balls(img_corrected,
         get_balls_coords(img_gray),
-        get_table_coords(img_gray))
+        table_contour)
 
     print(*balls.to_list(), sep='\n')
 
-    cv.imshow('res-color-test.png', img)
+    cv.imshow('res-color-test.png', img_corrected)
+    # cv.imwrite('corrected-perspective-gray.png', img_gray)
     cv.waitKey(0)
 
 
@@ -26,16 +30,14 @@ def load_image(name, grayscale=False):
     return cv.imread('snooker/' + name, iscolor)
 
 
-def mark_balls(image, balls_coords, table_coords):
+def mark_balls(image, balls_coords, table_contour):
     (w, h, points) = balls_coords
     balls = Balls()
     finder = ColorFinder()
 
-    table_contour = outline_table(image, table_coords)
-
     for i, pt in enumerate(points):
-        if is_outside_table(pt, table_contour):
-            continue
+        # if is_outside_table(pt, table_contour):
+        #     continue
 
         rect = get_rectangle(pt, w, h)
         color = finder.find(image, rect)
@@ -47,11 +49,15 @@ def mark_balls(image, balls_coords, table_coords):
 
 
 def get_balls_coords(image):
-    (w, h, m1) = find_matches(image, 'blue-top-std2.png')
-    (w, h, m2) = find_matches(image, 'pink-top-std2.png')
+    (w, h, m1) = find_matches(image, 'pink-top-prog-gray-corrected.png')
+    (w, h, m2) = find_matches(image, 'blue-top-prog-gray-corrected.png')
+    (w, h, m3) = find_matches(image, 'red-top-prog-gray-corrected.png')
+    # (w, h, m1) = find_matches(image, 'pink-top-corrected.png')
+    # (w, h, m2) = find_matches(image, 'pink-top-corrected.png')
 
     matches = list(zip(*m1[::-1]))
     matches.extend( list(zip(*m2[::-1])) )
+    matches.extend( list(zip(*m3[::-1])) )
 
     return ( w, h, loc.distinct_locations(matches) )
 
@@ -100,15 +106,34 @@ def find_matches(image, template, threshold=0.8):
 
 def get_rectangle(point, width, height):
     w4 = int(width/4)
+    h2 = int(height/2)
     pt0offset = point[0] + 3
-    pt1offset = point[1] + 3
+    pt1offset = point[1] + 5
 
     return np.array([
         (pt0offset, pt1offset),
         (pt0offset + w4, pt1offset),
-        (pt0offset + w4, pt1offset + height),
-        (pt0offset, pt1offset + height)
+        (pt0offset + w4, pt1offset + h2),
+        (pt0offset, pt1offset + h2)
     ])
+
+
+def correct_perspective(image, coords):
+    size = (758, 1440)
+    plane = np.array([
+        [0, size[1]-1],
+        [0, 0],
+        [size[0]-1, 0],
+        [size[0]-1, size[1]-1]
+    ])
+
+    h, status = cv.findHomography( coords, plane )
+
+    return cv.warpPerspective( image, h, size )
+
+    # cv.imshow('Corrected perspective', img_corrected)
+    # cv.imwrite('corrected-perspective.png', img_corrected)
+    # cv.waitKey(0)
 
 
 if __name__ == '__main__':
