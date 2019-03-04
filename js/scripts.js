4778ce35cef6b0a199ff3e6fa3e757a392d3cb6a
Marek Grzybek <it.doesnt.matter.blog@gmail.com>
2019-03-04 15:57:38 +0100

    Add debug sequence; show actual pos (in seconds) instead of index in sequence

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 svgTable = (function(){
+    debug_seq = [{"balls": [{"color": "yellow", "y": 109.70475354107649, "x": 11.48197435897436}, {"color": "red", "y": -0.5097804532577754, "x": 9.552230769230768}, {"color": "red", "y": 19.371657223796035, "x": 6.754102564102564}, {"color": "pink", "y": 31.91225637393768, "x": 1.5437948717948724}, {"color": "brown", "y": 109.09301699716713, "x": 0.09648717948717789}, {"color": "blue", "y": 67.90275637393768, "x": 0}, {"color": "red", "y": 29.465310198300273, "x": 0.28946153846153744}, {"color": "white", "y": 31.708344192634556, "x": -6.368153846153845}, {"color": "green", "y": 109.09301699716713, "x": -10.99953846153846}, {"color": "red", "y": 15.803194050991493, "x": -26.63046153846154}, {"color": "red", "y": 42.92351416430594, "x": -31.06887179487179}], "game_id": "video/ronnie-masters.mkv", "timestamp": 902}, {"balls": [{"color": "yellow", "y": 109.70475354107649, "x": 11.48197435897436}, {"color": "red", "y": -0.5097804532577754, "x": 9.552230769230768}, {"color": "red", "y": 19.371657223796035, "x": 6.754102564102564}, {"color": "pink", "y": 31.91225637393768, "x": 1.5437948717948724}, {"color": "brown", "y": 109.09301699716713, "x": 0.09648717948717789}, {"color": "blue", "y": 67.90275637393768, "x": 0}, {"color": "red", "y": 29.465310198300273, "x": 0.28946153846153744}, {"color": "pink", "y": 31.606388101983, "x": -6.271666666666665}, {"color": "green", "y": 109.09301699716713, "x": -10.99953846153846}], "game_id": "video/ronnie-masters.mkv", "timestamp": 903}, {"balls": [{"color": "yellow", "y": 109.80670963172804, "x": 11.48197435897436}, {"color": "red", "y": -0.5097804532577754, "x": 9.552230769230768}, {"color": "red", "y": 19.371657223796035, "x": 6.754102564102564}, {"color": "pink", "y": 31.91225637393768, "x": 1.5437948717948724}, {"color": "brown", "y": 109.09301699716713, "x": 0.09648717948717789}, {"color": "blue", "y": 67.90275637393768, "x": 0}, {"color": "red", "y": 29.465310198300273, "x": 0.28946153846153744}, {"color": "white", "y": 31.606388101983, "x": -6.368153846153845}, {"color": "green", "y": 109.09301699716713, "x": -10.99953846153846}, {"color": "green", "y": 72.28686827195467, "x": -31.06887179487179}], "game_id": "video/ronnie-masters.mkv", "timestamp": 904}, {"balls": [{"color": "yellow", "y": 109.70475354107649, "x": 11.48197435897436}, {"color": "red", "y": -0.5097804532577754, "x": 9.552230769230768}, {"color": "red", "y": 19.371657223796035, "x": 6.754102564102564}, {"color": "pink", "y": 31.91225637393768, "x": 1.5437948717948724}, {"color": "brown", "y": 109.09301699716713, "x": 0.09648717948717789}, {"color": "blue", "y": 67.90275637393768, "x": 0}, {"color": "red", "y": 29.465310198300273, "x": 0.28946153846153744}, {"color": "black", "y": 10.399521246458932, "x": 0.09648717948717789}, {"color": "white", "y": 31.606388101983, "x": -6.271666666666665}, {"color": "green", "y": 109.09301699716713, "x": -10.99953846153846}, {"color": "pink", "y": 43.7391628895184, "x": -20.93771794871795}, {"color": "red", "y": 15.803194050991493, "x": -26.63046153846154}, {"color": "red", "y": 42.71960198300283, "x": -31.06887179487179}], "game_id": "video/ronnie-masters.mkv", "timestamp": 905}, {"balls": [{"color": "yellow", "y": 109.70475354107649, "x": 11.48197435897436}, {"color": "red", "y": -0.5097804532577754, "x": 9.552230769230768}, {"color": "red", "y": 19.371657223796035, "x": 6.754102564102564}, {"color": "pink", "y": 31.91225637393768, "x": 1.5437948717948724}, {"color": "brown", "y": 109.09301699716713, "x": 0.09648717948717789}, {"color": "blue", "y": 67.90275637393768, "x": 0}, {"color": "red", "y": 29.465310198300273, "x": 0.28946153846153744}, {"color": "black", "y": 10.399521246458932, "x": 0.09648717948717789}, {"color": "white", "y": 31.606388101983, "x": -6.271666666666665}, {"color": "green", "y": 109.09301699716713, "x": -10.99953846153846}, {"color": "brown", "y": 42.82155807365439, "x": -20.262307692307694}, {"color": "red", "y": 15.803194050991493, "x": -26.63046153846154}, {"color": "red", "y": 42.61764589235126, "x": -31.06887179487179}], "game_id": "video/ronnie-masters.mkv", "timestamp": 906}];
+
     function mappedColor(color) {
         return color;
     }
 
     return {
         frame: null,
-        sequence: [],
+        sequence: debug_seq,
         d: function() {
             return this.frame.document;
         },
@@ -60,17 +62,17 @@ svgTable = (function(){
             var start = parseInt($("start").value);
             this.request(filename, start, frames);
         },
-        redraw: function(balls, seqIndex) {
+        redraw: function(balls, pos) {
             this.clearTable();
             this.addBalls(balls);
-            $("seq_pos").value = seqIndex;
+            $("seq_pos").value = pos;
         },
         play: function() {
             if (this.sequence.length > 1) {
                 disablePlayButton();
                 var firstPos = this.sequence[0];
 
-                this.redraw( firstPos.balls, 0 );
+                this.redraw( firstPos.balls, firstPos.timestamp );
                 this.playNext(1);
             }
         },
@@ -92,7 +94,7 @@ svgTable = (function(){
 
             var prom = new Promise((resolve) => {
                 setTimeout(function() {
-                    that.redraw( nextPos.balls, seqIndex );
+                    that.redraw( nextPos.balls, nextTimestamp );
                     resolve();
                 }, timeDiff * 1000);
             });
